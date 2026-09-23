0e556c9e24381c30750be36f8e34b6068f0fab3f
Karoliukas14 <kargec1@ktu.lt>
2026-09-23 18:25:42 +0300

    Showcases that the grams are 100 on default, enabled manual change to the food data, currently only work when grams are on 100

--- frontend/src/components/HomePopUp.jsx
+++ frontend/src/components/HomePopUp.jsx
@@ -9,6 +9,7 @@ function HomePopUp({onClose})
     const [showOptions, setShowOptions] = useState(false);
     const [selectedFood, setSelectedFood] = useState(null);
     const [amountGrams, setAmountGrams] = useState(100);
+    const [manualChange, setManualChange] = useState(false);
 
     useEffect(() => {
     fetch("http://localhost:8080/foods")
@@ -71,6 +72,10 @@ function HomePopUp({onClose})
                 <div>
                     <label>Grams:</label>
                     <input 
+                        value={amountGrams}
+                        onBlur={(e)=>
+                            setAmountGrams(e.target.value)
+                        }
                         onChange={(e)=>{
                             setAmountGrams(e.target.value)
                         }}
@@ -80,37 +85,61 @@ function HomePopUp({onClose})
                 <div>
                     <label>Calories:</label>
                     <input 
-                        
-                        value = {selectedFood ? selectedFood.calories*(amountGrams/100) : ""}
-                        onChange={(e) => {
-                            setSelectedFood({selectedFood, calories: e.target.value})
+                        type = "number"
+                        value = {selectedFood ? selectedFood.calories*(amountGrams/100) :  "" }
+                        onBlur={(e) => {
+                            setSelectedFood({...selectedFood, calories:e.target.value})
+                        }}
+                        onChange={(e)=>{
+                            const num = e.target.value
+                            setSelectedFood({...selectedFood, calories: num})
+                            setManualChange(true)
                         }}
                     />
+
                 </div>
                 <div>
                     <label>Carbs:</label>
                     <input 
-                        value = {selectedFood ? selectedFood.carbs*(amountGrams/100) : ""}
-                        onChange={(e) => {
-                            setSelectedFood({selectedFood, carbs: e.target.value})
+                        type = "number"
+                        value = {selectedFood ?  selectedFood.carbs*(amountGrams/100) : ""}
+                        onBlur={(e) => {
+                            setSelectedFood({...selectedFood, carbs: e.target.value})
+                        }}
+                        onChange={(e)=>{
+                            const num = e.target.value
+                            setSelectedFood({...selectedFood, carbs: num})
+                            setManualChange(true)
                         }}
                     />
                 </div>
                 <div>
                     <label>Protein:</label>
                     <input 
+                        type = "number"
                         value = {selectedFood ? selectedFood.protein*(amountGrams/100) : ""}
-                        onChange={(e) => {
-                            setSelectedFood({selectedFood, protein: e.target.value})
+                        onBlur={(e) => {
+                            setSelectedFood({...selectedFood, protein: e.target.value})
+                        }}
+                        onChange={(e)=>{
+                            const num = e.target.value
+                            setSelectedFood({...selectedFood, protein: num})
+                            setManualChange(true)
                         }}
                     />
                 </div>
                 <div>
-                    <label>Fat:</label>r
+                    <label>Fat:</label>
                     <input 
-                        value = {selectedFood ? selectedFood.fat*(amountGrams/100) : ""}
-                        onChange={(e) => {
-                            setSelectedFood({selectedFood, fat: e.target.value})
+                        type = "number"
+                        value = {selectedFood ? selectedFood.fat*(amountGrams/100):  ""}
+                        onBlur={(e) => {
+                            setSelectedFood({...selectedFood, fat: e.target.value})
+                        }}
+                        onChange={(e)=>{
+                            const num = e.target.value
+                            setSelectedFood({...selectedFood, fat: num})
+                            setManualChange(true)
                         }}
                     />
                 </div>
